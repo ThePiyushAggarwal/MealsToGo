0a19125e2f1b76dc238d01e951ef5734fccb17cc
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-06-10 10:29:14 +0530

    Basic Status Bar for android and iOS

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native'
+
+const isAndroid = Platform.OS === 'android'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.first}>
+          <Text>FBI open up!</Text>
+        </View>
+        <View style={styles.second}>
+          <Text>Jeex</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: isAndroid ? StatusBar.currentHeight : 0,
   },
-});
+  first: {
+    padding: 16,
+    backgroundColor: 'green',
+  },
+  second: {
+    backgroundColor: 'pink',
+    flex: 1,
+  },
+})
