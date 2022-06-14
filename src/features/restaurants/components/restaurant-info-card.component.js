1a7c4952eadbc80a8131a5388cddc663fd647916
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-06-14 20:44:01 +0530

    Restaurant States Open-Close added

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Text } from 'react-native';
 import { Card } from 'react-native-paper';
 import styled from 'styled-components/native';
 import { SvgXml } from 'react-native-svg';
 import star from '../../../../assets/star';
+import open from '../../../../assets/open';
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -24,27 +24,51 @@ const Title = styled.Text`
   color: ${(props) => props.theme.colors.ui.primary};
 `;
 
-const Rating = styled.View`
+const RatingStateContainer = styled.View`
   flex-direction: row;
+  justify-content: space-between;
   padding: ${(props) => props.theme.space[2]} 0;
 `;
 
-const Address = styled(Text)`
+const Rating = styled.View`
+  flex-direction: row;
+`;
+
+const RestaurantState = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const ClosedLabel = styled.Text`
+  color: red;
+`;
+
+const Icon = styled.Image`
+  width: 20px;
+  height: 20px;
+`;
+
+const PaddingSpace = styled.View`
+  padding-left: 16px;
+`;
+
+const Address = styled.Text`
   font-family: ${(props) => props.theme.fonts.body};
   font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 
+// Component
 const RestaurantsInfoCard = ({ restaurant = {} }) => {
   const {
     name = 'Some Restaurant',
-    //   icon,
+    icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
     photos = [
       'https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg',
     ],
     address = '100 some random street',
-    //   isOpenNow = true,
+    isOpenNow = true,
     rating = 4,
-    //   isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
 
   // This array consists of rating number of undefineds
@@ -55,11 +79,22 @@ const RestaurantsInfoCard = ({ restaurant = {} }) => {
       <RestaurantCardCover source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
-        <Rating>
-          {ratingsArray.map(() => (
-            <SvgXml xml={star} width={20} height={20} />
-          ))}
-        </Rating>
+        <RatingStateContainer>
+          <Rating>
+            {ratingsArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <RestaurantState>
+            {isClosedTemporarily && (
+              <ClosedLabel>CLOSED TEMPORARILY</ClosedLabel>
+            )}
+            <PaddingSpace />
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <PaddingSpace />
+            <Icon source={{ uri: icon }} />
+          </RestaurantState>
+        </RatingStateContainer>
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
