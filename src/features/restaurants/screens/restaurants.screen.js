5814efeec654559522ebfadc014b95f198d154d2
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-07-02 22:57:03 +0530

    added Spinner

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -4,9 +4,14 @@ import { FlatList } from 'react-native';
 import SearchBar from '../../../components/SearchBar';
 import RestaurantsInfoCard from '../components/restaurant-info-card.component';
 import RestaurantsContext from '../../../services/restaurants/restaurants.context';
+import { ActivityIndicator } from 'react-native-paper';
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
+`;
+
+const Spinner = styled(ActivityIndicator)`
+  flex: 1;
 `;
 
 const RestaurantsScreen = () => {
@@ -17,11 +22,15 @@ const RestaurantsScreen = () => {
         <SearchBar />
       </SearchContainer>
 
-      <FlatList
-        data={restaurants}
-        renderItem={({ item }) => <RestaurantsInfoCard restaurant={item} />}
-        keyExtractor={(item) => item.name}
-      />
+      {isLoading ? (
+        <Spinner animating={true} size="large" />
+      ) : (
+        <FlatList
+          data={restaurants}
+          renderItem={({ item }) => <RestaurantsInfoCard restaurant={item} />}
+          keyExtractor={(item) => item.name}
+        />
+      )}
     </>
   );
 };
