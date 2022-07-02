7446dbeb3d3c6cfab140078781e94acf149f94f2
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-07-02 20:18:03 +0530

    installed camelize

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,16 +1,18 @@
 import { mocks } from './mock';
+import camelize from 'camelize';
 
 export const restaurantsRequest = (location = '37.7749295,-122.4194155') => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
     if (!mock) {
-      reject('Not Founf!');
+      reject('Not Found!');
     }
     resolve(mock);
   });
 };
 
 restaurantsRequest()
+  .then((result) => camelize(result))
   .then((result) => {
     console.log(result);
   })
